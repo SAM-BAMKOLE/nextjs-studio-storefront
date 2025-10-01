9aac098a7b369c41d3720ba9e8c5800bbe5876ab
Samuel Olagoroye <olagoroyesamuel7@gmail.com>
2025-10-01 13:18:46 +0000

    Responsiveness fixed

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -6,7 +6,7 @@ import { MobileNav } from "./mobile-nav";
 export function SiteHeader() {
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background">
-      <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
+      <div className="container flex px-5 h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
         <MainNav />
         <MobileNav />
         <div className="flex flex-1 items-center justify-end space-x-4">
