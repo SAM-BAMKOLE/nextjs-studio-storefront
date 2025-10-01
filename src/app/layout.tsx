dd5fb3e7ffe534de2bcc4e3bd43e446533e2c8af
Samuel Olagoroye <olagoroyesamuel7@gmail.com>
2025-10-01 13:22:21 +0000

    page mx-auto fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
         <AuthProvider>
           <CartProvider>
             <SiteHeader />
-            <main className="flex-1 container py-8 px-5">
+            <main className="flex-1 container mx-auto py-8 px-5">
               {children}
             </main>
             <Toaster />
